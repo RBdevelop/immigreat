34f5aa64feea66602289f2cb479eaff13e9d6feb
Robert Balderas <robert@rbdevelop.com>
2015-02-27 16:47:45 -0600

    fixed devise view

--- app/assets/javascripts/app.controllers.js
+++ app/assets/javascripts/app.controllers.js
@@ -1,34 +1,17 @@
 angular.module('app.controllers', [])
-.controller('landingPageCtrl', function($scope, $state) {
-	$scope.goLanding = function() {
-		$state.go('')
-	}
+.controller('homeCtrl', function($scope, $state) {
+	$scope.goHome = function() {
+		$state.go('home');
+	};
+	console.log('THis is working');
 })
-// .controller('twitterButton', function(){
-// 	window.twttr = (function(d, s, id) {
-//     var t, js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) {
-//         return
-//     }
-//     js = d.createElement(s);
-//     js.id = id;
-//     js.src = "https://platform.twitter.com/widgets.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-//     return window.twttr || (t = {
-//         _e: [],
-//         ready: function(f) {
-//             t._e.push(f)
-//         }
-//     })
-// 	}(document, "script", "twitter-wjs"))
-// })
-// .controller('videoPlayer', function(){
-// 	function randInt(min, max) {
-//         return Math.floor(Math.random() * (max-min + 1) + min);
-//       }
-//       $(document).ready(function() {
-//         var videoStart = randInt(1, 9);
-//         var iframe = '<iframe width="853" height="480" src="https://www.youtube.com/embed/videoseries?list=PLwVuCj2NpVjqy7RCwh3mc_GxAf8-_prut&autoplay=1&index=' + videoStart + '" frameborder="0" allowfullscreen id="video"></iframe>';
-//         $('#video_container').html(iframe);
-//       });
-// })
+.controller('tweetCtrl', function($scope, $state) {
+	$scope.goTweet = function() {
+		$state.go('tweet');
+	};
+})
+.controller('successCtrl', function($scope, $state) {
+	$scope.goSuccess = function() {
+		$state.go('success');
+	};
+});
