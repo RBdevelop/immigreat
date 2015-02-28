43295c11fbbd2555197503be28497f7e9a59c1d5
Robert Balderas <robert@rbdevelop.com>
2015-02-28 14:04:23 -0600

    updated controller to only respond with json

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,10 +28,8 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
-        format.html { render :new }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +40,8 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
         format.json { render :show, status: :ok, location: @tweet }
       else
-        format.html { render :edit }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
@@ -56,7 +52,6 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :users
-
+	validates :message , presence: true 
 	validates :message, length: ( maximum: 140,
 	  too_long :"% Keep it brief AND let Abbot have it!" )
 
