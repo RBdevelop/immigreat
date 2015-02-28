fe0bfcb5c3b67618324d45ac1979fd16052b5115
Robert Balderas <robert@rbdevelop.com>
2015-02-28 13:47:11 -0600

    validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ActiveRecord::Base
+	belongs_to :users
+
+	validates :message, length: ( maximum: 140,
+	  too_long :"% Keep it brief AND let Abbot have it!" )
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  has_many :tweets
+
+  validates :first_name , presence: true
+  validates :last_name, presence: true
+  validates_uniqueness_of :twitter_handle, :presence => true
+
 end
