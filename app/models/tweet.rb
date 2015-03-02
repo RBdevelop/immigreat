b68c57502f34d2b66623a25c4b0fb3a09146f42f
Robert Balderas <robert@rbdevelop.com>
2015-03-01 18:44:41 -0600

    adding devise parameter twitter handle

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,8 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :users
-	validates :message , presence: true 
-	validates :message, length: ( maximum: 140,
+	validates :message,
+	:presence => true,
+	:length => ( maximum: 140,
 	  too_long :"% Keep it brief AND let Abbot have it!" )
 
 end
