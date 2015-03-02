22cbea3ed71bae4c499ca309d7001a462644c77b
Robert Balderas <robert@rbdevelop.com>
2015-03-01 22:10:28 -0600

    fix tweet validator

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,10 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :users
-	validates :message, :presence => true,
-	#:length => ( maximum: 140,
-	 # too_long :"% Keep it brief AND let Abbot have it!" )
+	validates :message,
+	:presence => true,
+	:length: { 
+		minimum :2,
+		maximum: 140,
+	  	too_long: "% Keep it brief AND let Abbot have it!" )
 
 end
